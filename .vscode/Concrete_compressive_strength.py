d42c688be89a9d950d8a331927c66d3964f49c14
Wolf <u7040076@anu.edu.au>
2020-09-06 00:29:09 +1000

    commit

--- .vscode/Concrete_compressive_strength.py
+++ .vscode/Concrete_compressive_strength.py
@@ -7,5 +7,4 @@ file_location = "F:\desktop\Assignment\Concrete_Data.xls"
 data = xlrd.open_workbook(file_location)
 sheet = data.sheet_by_index(0)
 
-
 data = [[sheet.cell_value(r,c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
