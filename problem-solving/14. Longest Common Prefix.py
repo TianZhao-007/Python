e5f000b2e7beeb278e15d1d484001a4669aa9811
TianZhao-007 <u7040076@anu.edu.au>
2020-09-23 01:52:33 +1000

    commit

--- problem-solving/14. Longest Common Prefix.py
+++ problem-solving/14. Longest Common Prefix.py
@@ -1,24 +1,47 @@
+
+################# Not right solution
 # class Solution(object):
 
-a = ["dog","racecar","car"]
+# a = ["a","aa"]
 
-def longestCommonPrefix(strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        min = len(strs[0])
-        for i in range(len(strs)):
-            if (len(strs[i-1])>len(strs[i])):
-                min = len(strs[i])
+# def longestCommonPrefix(strs):
+#         compre = ""
+#         if(len(strs)) == 0: return compre
+
+#         # compute the min length of strs elements 
+#         min = len(strs[0])
+#         for i in range(len(strs)-1):
+#             if (len(strs[i-1])>len(strs[i])):
+#                 min = len(strs[i])
 
 
-        for j in range(min):
-            for i in range(1,len(strs)):
-                if strs[0][j] != strs[i][j]:
-                    break
-        
-        if(j == 0): return ""
-        else: return strs[0][0:j-1]
+#         for j in range(min):
+#             for i in range(1,len(strs)):
+#                 if strs[0][j] != strs[i][j]:
+#                     return compre
+#             compre = compre + strs[0][j]
+#         return compre
 
-print(longestCommonPrefix(a))
+
+
+# print(longestCommonPrefix(a))
+
+
+
+############### leetcode reference solution
+
+class Solution(object):
+
+    def longestCommonPrefix(self, strs):
+	commonPre = ""
+
+	if len(strs)== 0:
+		return commonPre
+
+	for i in range(len(strs[0])):
+		for j in range (1,len(strs)):
+			if (i >= len(strs[j]) or strs[j][i] != strs[0][i]):
+				return commonPre
+		commonPre = commonPre + strs[0][i]
+
+	return commonPre
