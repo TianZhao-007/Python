5fdeb443a15635962b057f9220b695f407ea19f8
TianZhao-007 <u7040076@anu.edu.au>
2020-09-30 01:03:29 +1000

    commit

---
+++ problem-solving/69. Sqrt(x).py
@@ -0,0 +1,13 @@
+# My answer 1:
+
+# import math
+# class Solution(object):
+#     def mySqrt(self, x):
+#         """
+#         :type x: int
+#         :rtype: int
+#         """
+        
+#         return math.sqrt(x)
+
+# Answer 2: binary search
